84bf29c962b8d265268e0a041820b79a65bfc443
Evgeniy Kozhich <wanderbon95@gmail.com>
2022-04-04 12:29:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export const rules = {
+  'no-cycle-alias': require('./rules/no-cycle-alias'),
+};
+
+export const configs = {
+  'recommended': require('../config/recommended'),
+
+  'errors': require('../config/errors'),
+  'warnings': require('../config/warnings'),
+
+  // shhhh... work in progress "secret" rules
+  'stage-0': require('../config/stage-0'),
+
+  // useful stuff for folks using various environments
+  'react': require('../config/react'),
+  'react-native': require('../config/react-native'),
+  'electron': require('../config/electron'),
+  'typescript': require('../config/typescript'),
+};
